3170aa7bc2c3c7c20ce5b83383cfaa5937b430b2
coherent17 <mnb51817@gmail.com>
2023-10-25 18:13:41 +0800

    init Lab2

---
+++ Lab2_Static-Timing-Analysis/include/Circuit.h
@@ -0,0 +1,15 @@
+#ifndef _CIRCUIT_H_
+#define _CIRCUIT_H_
+
+#include "Cell.h"
+#include "Net.h"
+
+struct Circuit{
+
+    Circuit();
+    ~Circuit();
+
+    void Parser();
+};
+
+#endif
---
+++ Lab2_Static-Timing-Analysis/include/Util.h
@@ -0,0 +1,16 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+enum Cell_Type{
+    START = 0,
+    END = 1,
+    NAND = 2,
+    NOR = 3,
+    INV = 4
+};
+
+enum Net_Type{
+    
+}
+
+#endif
